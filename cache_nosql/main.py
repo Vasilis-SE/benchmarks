f028026c7abb5e122f4f4bbc3b366c707ba98785
Vasilis CodeX <vasilis.tris@gmail.com>
2023-02-11 15:23:05 +0200

    dragonflydb and redis benchmark implementation class & dataset

--- cache_nosql/main.py
+++ cache_nosql/main.py
@@ -1,15 +1,53 @@
+import json
+import time
+
 import dragonfly_bench
 import redis_bench
 
 if __name__ == "__main__":
     _redis = redis_bench.RedisBenchmarkClass()
-    _dragon_http = dragonfly_bench.DragonflyBenchmarkClass()
-
+    _dragonfly = dragonfly_bench.DragonflyBenchmarkClass()
+    
     try:
+        times = 10
+        
         _redis.connect()
+        _dragonfly.connect()
         
-        _dragon_http.add_data("my-key", "my-value")
+        names_ds = open('../datasets/names.json')
+        names = json.load(names_ds)
+
+        # Redis 100 registries set
+        sum_benchmarks = 0
+        for j in range(0, times):
+            start = time.time()
+            for i in range(0,100):
+                _redis.set(names[i].get('_id'), names[i].get('name'))
+            end = time.time()
+            time_seconds = end - start
+            
+            sum_benchmarks += time_seconds
+            _redis.flush()
+
+        avg_bench_exec_sec = sum_benchmarks / times
+        print("The average exectution time for set function on redis is: {} s".format(str(avg_bench_exec_sec)))
         
+        
+        # Dragonfly 100 registries set
+        sum_benchmarks = 0
+        for j in range(0, times):
+            start = time.time()
+            for i in range(0,100):
+                _dragonfly.set(names[i].get('_id'), names[i].get('name'))
+            end = time.time()
+            time_seconds = end - start
+            
+            sum_benchmarks += time_seconds
+            _dragonfly.flush()
+
+        avg_bench_exec_sec = sum_benchmarks / times
+        print("The average exectution time for set function on dragonfly is: {} s".format(str(avg_bench_exec_sec)))
+         
     except Exception as e:
         print("[ERROR] " + str(e))
         
--- cache_nosql/redis_bench.py
+++ cache_nosql/redis_bench.py
@@ -9,3 +9,14 @@ class RedisBenchmarkClass:
         self.connector = redis.Redis(
             host='localhost',
             port=8743)
+        
+    def set(self, key, value):
+        self.connector.set(key, value)
+        
+    def get(self, key):
+        self.connector.get(key)
+        
+    def flush(self):
+        self.connector.flushall()
+
+
